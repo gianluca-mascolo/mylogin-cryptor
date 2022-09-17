13bf221416b806d0abea348dd061838275ab0824
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-09-17 16:29:23 +0200

    shellcheck

--- mylogin_encoder.sh
+++ mylogin_encoder.sh
@@ -41,7 +41,7 @@ shift $((OPTIND - 1))
 [ "${MYLOGIN_FILE:+is_set}" = "is_set" ] || MYLOGIN_FILE="${MYSQL_TEST_LOGIN_FILE:-$HOME/.mylogin.cnf}"
 KeyPress="x"
 if [ -f "$MYLOGIN_FILE" ] && [ "${OVERWRITE_MYLOGIN:-n}" != "overwrite" ]; then {
-    read -p "$MYLOGIN_FILE already exist. Do you want to overwrite it? (y/n)" -s -n1 -t10 KeyPress
+    read -r -p "$MYLOGIN_FILE already exist. Do you want to overwrite it? (y/n)" -s -n1 -t10 KeyPress
     [ "$KeyPress" != "y" ] && exit 0
     echo ""
 }
@@ -55,13 +55,16 @@ for ((i=0;i<20;i++)); do {
     HEX_KEY[$((i%16))]=$((${HEX_KEY[$((i%16))]:-0}^BYTE))
 }
 done
+# shellcheck disable=SC2059
 printf "\x00\x00\x00\x00${ENCRYPTED_KEY}" > "$MYLOGIN_FILE"
 OPENSSL_KEY="$(printf %02x "${HEX_KEY[@]}")"
-while read line; do {
+while read -r line; do {
     ENCRYPTED_LINE="$(echo "$line" | openssl enc -e -aes-128-ecb -K "$OPENSSL_KEY" | od -An -tu1 -w1 -v | tr -s '[:space:]' ' ')"
+    # shellcheck disable=SC2086
     ENCRYPTED_LINE="$(printf '\\x%02x' $ENCRYPTED_LINE)"
     ENCRYPTED_LENGTH="$(echo -nE "$ENCRYPTED_LINE" | wc -c)"
     ENCRYPTED_LENGTH="$((ENCRYPTED_LENGTH / 4))"
+    # shellcheck disable=SC2059
     printf "\x$(printf %02x $ENCRYPTED_LENGTH)\x00\x00\x00${ENCRYPTED_LINE}" >> "$MYLOGIN_FILE"
 }
 done < "${PLAINTEXT_FILE:-/dev/stdin}"
